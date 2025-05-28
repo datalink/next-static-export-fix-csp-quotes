fde7f0bb020922de6bd8a2343586f5f313b0448e
Scott Davey <sdavey@datalink.com.au>
2025-05-28 16:42:54 +1000

    Removed verbose output

--- cli.js
+++ cli.js
@@ -1,26 +1,28 @@
 #!/usr/bin/env node
-var fs = require("fs");
-var path = require("path");
+const fs = require("fs");
+const path = require("path");
 
-var dirPath = "./out";
+const dirPath = "./out";
 
 // Matches &#x27; within the content="" section of the content security policy tag
-var REGEX = /(\"Content-Security-Policy\" content=\".*?)(&#x27;)(.*?\")/gm;
+const REGEX = /(\"Content-Security-Policy\" content=\".*?)(&#x27;)(.*?\")/gm;
 
-console.log("Scanning dirdctory " + dirPath + " for html files");
+console.log("Scanning directory " + dirPath + " for html files...");
+let numFilesScanned = 0;
+let numMatches = 0;
 fs.readdirSync(dirPath)
     .filter((file) => file.substr(file.length - 5) === '.html')
     .forEach((file) => {
-        console.log("Updating " + file);
+        numFilesScanned++;
         let contents = fs.readFileSync(dirPath + '/' + file).toString();
         let i = 0;
-        let loop = REGEX.test(contents);;
+        let loop = REGEX.test(contents);
         while (loop) {
             contents = contents.replace(REGEX, "$1'$3");
             loop = REGEX.test(contents);
-            i++;
+            numMatches++;
         }
-        console.log("Fixed " + i + " occurrances");
         fs.writeFileSync(dirPath + '/' + file, contents);
     });
+console.log(`Processed ${numFilesScanned} files, fixed ${numMatches} issues.`);
 console.log("Done");
